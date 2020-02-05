60a5f9913b212f495f8060dd56c8094c62289d14
tyalimov <lazmible@mail.ru>
2020-02-05 21:45:49 +0300

    [codedrop] Added tests for HashMaker

---
+++ signature_builder/signature_builder_tests/hash_maker_tests.cpp
@@ -0,0 +1,90 @@
+#include <gtest/gtest.h>
+
+#include <utils.h>
+#include <hash_maker.h>
+
+#include <vector>
+#include <fstream>
+
+using namespace builder;
+
+using utils::Path;
+
+std::string ToHex(const utils::BinaryBuffer& input)
+{
+    static const char *const lut = "0123456789abcdef";
+    size_t len = input.size();
+
+    std::string output;
+    output.reserve(2 * len);
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        const uint8_t c = input[i];
+        output.push_back(lut[c >> 4]);
+        output.push_back(lut[c & 15]);
+    }
+
+    return output;
+}
+
+std::string ReadFile(const Path& file)
+{
+    std::ifstream ss(file.generic_string(), std::ios::binary);
+    std::stringstream buffer;
+    buffer << ss.rdbuf();
+
+    return buffer.str();
+}
+
+utils::BinaryBuffer ToBinaryBuffer(const std::string& data)
+{
+    return utils::BinaryBuffer{ data.begin(), data.end() };
+}
+
+void TestHashMaker(const std::string& expected, const Path& file) try
+{
+    Path filename = "hash_maker";
+    filename /= file;
+
+    EXPECT_TRUE(utils::fs::exists(filename));
+
+    auto file_data     = ReadFile(filename.generic_string());
+    auto buffer        = std::make_shared<utils::BinaryBuffer>(ToBinaryBuffer(file_data));
+    auto hash          = crypto::HashMaker(buffer).getHash();
+
+    EXPECT_EQ(ToHex(*hash), expected);
+}
+catch (const std::exception& ex)
+{
+    std::cerr << ex.what() << std::endl;
+    FAIL();
+}
+
+TEST(hash_maker, Zero_bytes)
+{
+    std::string expected{ "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e" };
+
+    TestHashMaker(expected, "zero_bytes.txt");
+}
+
+TEST(hash_maker, Half_block)
+{
+    std::string expected{ "d7f7b4b1862fe2d4da0f0ee0cf69aa00291a11cc6bfddcbb3dd7f30a608864fe6ecaf9cb59f334138d6d6981ceaa3742890a2b30d077634483396429edf53ff0" };
+
+    TestHashMaker(expected, "half_block.txt");
+}
+
+TEST(hash_maker, Full_block)
+{
+    std::string expected{ "bacd075be4216b75875ace1f2bcd96f664c50894a804690669fd45df7874fa24e0d017effb5917dbb597d4c2c873b2ff195e7d6d27236e32fceb9fcb6ce33309" };
+
+    TestHashMaker(expected, "full_block.txt");
+}
+
+TEST(hash_maker, More_than_full_block)
+{
+    std::string expected{ "66446782019e74034038f4b052926fc239bb51fa2e77e5e31b39907822ce9e1d0d43e6fc9e4095fd9ee3c6d394c816aec869d2c4de4643e4ef8aacc183fecb70" };
+    
+    TestHashMaker(expected, "more_than_full_block.txt");
+}
