b2098911f3b3a0d2c8ee31c52c94ffc3221579d7
tyalimov <lazmible@mail.ru>
2020-02-08 19:26:31 +0300

    [codedrop] Added task queue

---
+++ signature_builder/signature_builder_lib/task_queue.cpp
@@ -0,0 +1,70 @@
+﻿#include "task_queue.h"
+#include "hash_maker.h"
+
+namespace builder::threading
+{
+    void HashTask::perform()
+    {
+        block.hash = crypto::HashMaker(block.buffer).getHash();
+        block.buffer.reset();
+    }
+
+    TaskQueue::TaskQueue()
+        : m_queue()
+        , m_cv()
+        , m_mutex()
+        , m_no_more_push(false)
+    {
+    }
+
+    bool TaskQueue::tryPop(HashTask& task)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+
+        m_cv.wait(lock, [&]()
+            {
+                return m_no_more_push || !this->m_queue.empty();
+            });
+
+        if (m_no_more_push)
+        {
+            return false;
+        }
+
+        task = m_queue.front();
+        m_queue.pop();
+
+        return  true;
+    }
+
+    void TaskQueue::push(const HashTask& task)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        if (!m_no_more_push)
+        {
+            m_queue.push(task);
+            m_cv.notify_one();
+        }
+    }
+
+    void TaskQueue::stop()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        m_no_more_push = true;
+        m_cv.notify_all();
+    }
+
+    bool TaskQueue::stopped() 
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        return m_no_more_push;
+    }
+
+    TaskQueue::SizeType TaskQueue::size() const
+    {
+        return m_queue.size();
+    }
+}
---
+++ signature_builder/signature_builder_lib/task_queue.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "utils.h"
+
+#include <queue>
+#include <mutex>
+
+namespace builder::threading
+{
+    struct HashTask
+    {
+        utils::BinaryBlock block;
+
+        void perform();
+    };
+
+    class TaskQueue
+    {
+    private:
+        std::queue<HashTask>         m_queue;
+        std::condition_variable  m_cv;
+        std::mutex               m_mutex;
+        bool                     m_no_more_push;
+      
+    public:
+
+        using SizeType = std::queue<HashTask>::size_type;
+
+        TaskQueue();
+
+        bool tryPop(HashTask& task);
+        void push(const HashTask& task);
+        void stop();
+
+        [[nodiscard]] bool stopped();
+        [[nodiscard]] SizeType size() const;
+    };
+}
